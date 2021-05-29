24e137f078b40813340f543130d8dd7ddf15555c
hoseindig <hosein_dig@yahoo.com>
2021-05-29 11:18:57 +0430

    init

---
+++ precache-manifest.929e5c387c343b08eca2bb0cc32e9f2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd96d14352ed60f25e4",
+    "url": "/css/chunk-vendors.594211b0.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "a63605bda5e990a6b6e57e253efde92b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f795fdaa9cb8a1d90a75",
+    "url": "/js/about.353eaf48.js"
+  },
+  {
+    "revision": "969b1a4fce120735d415",
+    "url": "/js/app.d4eb5d7d.js"
+  },
+  {
+    "revision": "2bd96d14352ed60f25e4",
+    "url": "/js/chunk-vendors.845953a9.js"
+  },
+  {
+    "revision": "36600080547539fc9c4b4980d1700932",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
